031dc7cdfb6726d16a3607bc6be884904b229f7e
poiu <ponpottboomk@gmail.com>
2025-11-30 13:41:07 +0700

    Add main React app structure and movie components
    Migrated the app to React, introducing core components for layout, authentication, header, home, recommended movies, movie streaming, and review functionality. Updated main.jsx to use ReactDOM and React Router, and removed unused style.css. This sets up the foundation for a movie streaming and review platform with protected routes and admin review features.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,19 @@
-import { createApp } from 'vue'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './style.css'
 import App from './App.jsx'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthProvider.jsx';
 
-createApp(App).mount('#app')
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path ="/*" element ={<App/>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  </StrictMode>,
+)
